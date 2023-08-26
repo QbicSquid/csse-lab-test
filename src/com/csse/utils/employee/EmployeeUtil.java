d9cda45bae3939cb847eb50f82286d1e81cfb73c
QbicSquid <shifan611@gmail.com>
2023-08-26 15:18:53 +0530

    Added exception handling

--- src/com/csse/utils/employee/EmployeeUtil.java
+++ src/com/csse/utils/employee/EmployeeUtil.java
@@ -1,30 +1,41 @@
 package com.csse.utils.employee;
 
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.TransformerException;
-
 import com.csse.config.Config;
+
 import java.io.File;
-import org.xml.sax.SAXException;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.NodeList;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
-import javax.xml.transform.TransformerConfigurationException;
 
 public class EmployeeUtil extends Config {
-	
+
+	private static final Logger logger = Logger.getLogger("Main");
+
 	public static String getEmployeeById(String id) throws Exception {
-		NodeList n; Element e = null;
-		n = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-				.parse(new File("src/resources/EmployeeQuery.xml"))
-				.getElementsByTagName("query");
-		for (int x = 0; x < n.getLength(); x++) {
-			e = (Element) n.item(x);
-			if (e.getAttribute("id").equals(id))
-				break;
+		try {
+			NodeList n;
+			Element e = null;
+			n = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+					.parse(new File("src/resources/EmployeeQuery.xml")).getElementsByTagName("query");
+			for (int x = 0; x < n.getLength(); x++) {
+				e = (Element) n.item(x);
+				if (e.getAttribute("id").equals(id))
+					break;
+			}
+			return e.getTextContent().trim();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, "Failed to read query file - " + e.getMessage());
+		} catch (ParserConfigurationException e) {
+			logger.log(Level.SEVERE, "Failed to parse employee query - " + e.getMessage());
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Failed to get employee - " + e.getMessage());
 		}
-		return e.getTextContent().trim();
+		return "";
 	}
 }
