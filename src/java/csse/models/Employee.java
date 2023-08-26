328d21f6f40858edd425a20ffbf8048512f06954
Laksara <it21092824@my.sliit.lk>
2023-08-26 14:49:58 +0530

    updated employeeclass

--- src/java/csse/models/Employee.java
+++ src/java/csse/models/Employee.java
@@ -2,53 +2,114 @@ package java.csse.models;
 
 public class Employee {
 
-	public String iDeMPLOYEE;
-	public String nAMEfULL;
-	public String aDDRESS;
-	public String fACULTYnAME;
-	public String dEPARTMENT;
-	public String dESIGNATION;
+	public String getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFacultyName() {
+		return facultyName;
+	}
+
+	public void setFacultyName(String facultyName) {
+		this.facultyName = facultyName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String employeeID;
+	public String fullName;
+	public String address;
+	public String facultyName;
+	public String department;
+	public String designation;
+
 	public String e1() {
-		return iDeMPLOYEE;
+		return employeeID;
 	}
+
 	public void e1(String employeeID) {
-		iDeMPLOYEE = employeeID;
+		this.employeeID = employeeID;
 	}
+
 	public String e2() {
-		return nAMEfULL;
+		return fullName;
 	}
+
 	public void e2(String fullName) {
-		nAMEfULL = fullName;
+		this.fullName = fullName;
 	}
+
 	public String e3() {
-		return aDDRESS;
+		return address;
 	}
+
 	public void e3(String address) {
-		aDDRESS = address;
+		this.address = address;
 	}
+
 	public String e4() {
-		return fACULTYnAME;
+		return facultyName;
 	}
+
 	public void e4(String facultyName) {
-		fACULTYnAME = facultyName;
+		this.facultyName = facultyName;
 	}
+
 	public String e5() {
-		return dEPARTMENT;
+		return department;
 	}
+
 	public void e5(String department) {
-		dEPARTMENT = department;
+		this.department = department;
 	}
+
 	public String e6() {
-		return dESIGNATION;
+		return designation;
 	}
+
 	public void e6(String designation) {
-		dESIGNATION = designation;
+		this.designation = designation;
 	}
+
 	@Override
 	public String toString() {
-		
-		return "Employee ID = " + iDeMPLOYEE + "\n" + "FullName = " + nAMEfULL + "\n" + "Address = " + aDDRESS + "\n"
-				+ "Faculty Name = " + fACULTYnAME + "\n" + "Department = " + dEPARTMENT + "\n" + "Designation = "
-				+ dESIGNATION;
+
+		return "Employee ID = " + employeeID + "\n" + "FullName = " + fullName + "\n" + "Address = " + address + "\n"
+				+ "Faculty Name = " + facultyName + "\n" + "department = " + department + "\n" + "designation = "
+				+ designation;
 	}
 }
